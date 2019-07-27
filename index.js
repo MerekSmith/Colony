fd91e1b036bf7a491cb9472c6e099f94a095cf7a
Merek Smith <mereksmith@hotmail.com>
2019-07-27 11:58:59 -0600

    fix for heroku

--- index.js
+++ index.js
@@ -26,11 +26,18 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-
 require("./routes/authRoutes")(app);
+
+if (process.env.NODE_ENV === "production") {
+  // Express will serve up production assets like our main.js file, or main.css file.
+  app.use(express.static("client/build"));
+
+  // Express will serve up the index.html file. If it does not recogonize the route.
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
